efd190a15689fd1c346a0b4f5afb143da6f2595a
laggingreflex <laggingreflex@gmail.com>
2018-11-20 09:14:15 +0530

    feat: off (removeEventListeners)

--- index.js
+++ index.js
@@ -1,43 +1,44 @@
 const crypto = require('crypto');
 const symbol = '__fork-acknowledge__';
 
-const uid = () => crypto.randomBytes(16).toString('base64');
-
-const queue = {};
-
-const on = process => fn => process.on('message', async message => {
-  if (!message || message.symbol !== symbol) { return; }
-  try {
-    message.result = await fn(...message.args);
-  } catch (error) {
-    message.error = { message: error.message, stack: error.stack };
-  }
-  process.send(message);
-});
-
-const send = process => {
-  const send = (...args) => {
-    const id = uid();
-    process.send({ symbol, id, args });
-    return new Promise((resolve, reject) => {
-      queue[id] = { resolve, reject };
-    });
-  };
-  process.on('message', message => {
+const send = (process, opts = {}) => (...args) => {
+  const id = crypto.randomBytes(16).toString('base64');
+  const promise = { id };
+  let off;
+  const on = message => {
     if (!message || message.symbol !== symbol) { return; }
     const { id, result, error } = message;
-    if (!queue[id]) return;
-    const { resolve, reject } = queue[id];
-    delete queue[id];
+    if (promise.id !== id) return;
+    delete promise[id];
     if (error) {
       const e = new Error(error.message);
       e.stack = error.stack;
-      reject(e);
+      promise.reject(e);
     } else {
-      resolve(result);
+      promise.resolve(result);
     }
+    off();
+  };
+  process.on('message', on);
+  off = () => process.off('message', on);
+  process.send({ symbol, id, args });
+  return new Promise((resolve, reject) => {
+    Object.assign(promise, { resolve, reject });
   });
-  return send;
 }
 
-module.exports = process => ({ on: on(process), send: send(process) });
+const on = (process, opts = {}) => fn => {
+  const on = async message => {
+    if (!message || message.symbol !== symbol) { return; }
+    try {
+      message.result = await fn(...message.args);
+    } catch (error) {
+      message.error = { message: error.message, stack: error.stack };
+    }
+    process.send(message);
+  };
+  process.on('message', on);
+  return () => process.off('message', on);
+};
+
+module.exports = (process, opts) => ({ send: send(process, opts), on: on(process, opts) });
